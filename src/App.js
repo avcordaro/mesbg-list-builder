1e81de0550f2058b9e94811f33e8b2dd5c8f8045
avcordaro <alex_cordaro@hotmail.co.uk>
2023-12-30 19:50:50 +0000

    Minor bugfixes

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 export default function App() {
-  const VERSION = "3.1.0"
+  const VERSION = "3.1.1"
   const faction_lists = {
     "Good Army": new Set(mesbg_data.filter(data => data.faction_type == "Good Army").map((data) => data.faction)),
     "Evil Army": new Set(mesbg_data.filter(data => data.faction_type == "Evil Army").map((data) => data.faction)),
@@ -363,7 +363,7 @@ export default function App() {
                 MESBG List Builder
               </p>
               <p className="p-0 m-0" style={{ fontSize: "16px" }}>
-                version {VERSION} (updated 01-Dec-23)
+                version {VERSION} (updated 30-Dec-23)
               </p>
             </Stack>
             <h6 className="mb-0" style={{ marginLeft: "30px"}}>Total Points: <b>{roster.points}</b></h6>
