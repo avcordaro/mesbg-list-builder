c10e7004530bab1099ebb473e615188363a7a8d3
Alex Cordaro <alex_cordaro@hotmail.co.uk>
2024-02-29 08:59:13 +0000

    Warg Rider profile

--- src/App.js
+++ src/App.js
@@ -42,8 +42,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 export default function App() {
-  const VERSION = "4.1.7"
-  const UPDATED = "28-Feb-2024"
+  const VERSION = "4.1.8"
+  const UPDATED = "29-Feb-2024"
   const faction_lists = {
     "Good Army": new Set(mesbg_data.filter(data => data.faction_type == "Good Army").map((data) => data.faction)),
     "Evil Army": new Set(mesbg_data.filter(data => data.faction_type == "Evil Army").map((data) => data.faction)),
