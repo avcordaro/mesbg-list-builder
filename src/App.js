7d02e09ff028263b485ac9d1cf82b71212bcda53
Alex Cordaro <alex_cordaro@hotmail.co.uk>
2024-03-29 20:22:12 +0000

    Herald of the Dead correction

--- src/App.js
+++ src/App.js
@@ -42,8 +42,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 export default function App() {
-  const VERSION = "4.2.7"
-  const UPDATED = "26-Mar-2024"
+  const VERSION = "4.2.8"
+  const UPDATED = "29-Mar-2024"
   const faction_lists = {
     "Good Army": new Set(mesbg_data.filter(data => data.faction_type == "Good Army").map((data) => data.faction)),
     "Evil Army": new Set(mesbg_data.filter(data => data.faction_type == "Evil Army").map((data) => data.faction)),
