5a7eb4b577ddc016083bf1be499bfd503b50d431
Alex Cordaro <alex_cordaro@hotmail.co.uk>
2024-05-07 11:45:39 +0100

    Bolt thrower and Gil-galad fix

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -8,8 +8,8 @@ import { FaHammer } from "react-icons/fa6";
 import React from "react";
 import {Navbar} from "react-bootstrap";
 
-export const VERSION = "5.0.3";
-const UPDATED = "06-May-2024";
+export const VERSION = "5.0.4";
+const UPDATED = "07-May-2024";
 
 /* Navbar component that displays at the top of the page. */
 
