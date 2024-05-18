7541e6ed8fda69eb717d13d564382d79739f4531
Alex Cordaro <alex_cordaro@hotmail.co.uk>
2024-05-18 09:11:21 +0100

    Thranduil correction

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -8,8 +8,8 @@ import { FaHammer } from "react-icons/fa6";
 import React from "react";
 import {Navbar} from "react-bootstrap";
 
-export const VERSION = "5.2.2";
-const UPDATED = "16-May-2024";
+export const VERSION = "5.2.3";
+const UPDATED = "18-May-2024";
 
 /* Navbar component that displays at the top of the page. */
 
