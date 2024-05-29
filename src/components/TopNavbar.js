eb65fa1acf75fa716676f047957b60a84675577c
Alex Cordaro <alex_cordaro@hotmail.co.uk>
2024-05-29 22:18:52 +0100

    version increment

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -8,8 +8,8 @@ import { FaHammer } from "react-icons/fa6";
 import React from "react";
 import {Navbar} from "react-bootstrap";
 
-export const VERSION = "5.2.7";
-const UPDATED = "23-May-2024";
+export const VERSION = "5.2.8";
+const UPDATED = "29-May-2024";
 
 /* Navbar component that displays at the top of the page. */
 
