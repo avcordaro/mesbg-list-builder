9d7be7b4048fc0b8916a4d0a28e01291e2cca5a6
Alex Cordaro <alex_cordaro@hotmail.co.uk>
2024-08-01 20:11:53 +0100

    Galadhrim Knight correction

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -9,8 +9,8 @@ import React from "react";
 import {Navbar} from "react-bootstrap";
 import logo from '../images/logo.svg';
 
-export const VERSION = "5.3.8";
-const UPDATED = "29-Jul-2024";
+export const VERSION = "5.3.9";
+const UPDATED = "01-Aug-2024";
 
 /* Navbar component that displays at the top of the page. */
 
