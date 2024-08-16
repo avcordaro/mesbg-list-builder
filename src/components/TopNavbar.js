e421c142d37cc5fc7f5f35e9810f19dea81b06a4
Alex Cordaro <alex_cordaro@hotmail.co.uk>
2024-08-17 00:48:19 +0100

    new edition message

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,16 +1,19 @@
 import Stack from "react-bootstrap/Stack";
+import Badge from "react-bootstrap/Badge"
+import Offcanvas from "react-bootstrap/Offcanvas";
 import {MdReportGmailerrorred} from "react-icons/md";
 import Button from "react-bootstrap/Button";
 import {FaTableList} from "react-icons/fa6";
 import {BiLinkAlt, BiSolidFileImport} from "react-icons/bi";
 import {FaRegCopyright, FaChessRook} from "react-icons/fa";
+import { FaQuestion } from "react-icons/fa6";
 import { FaHammer } from "react-icons/fa6";
-import React from "react";
+import React, {useState} from "react";
 import {Navbar} from "react-bootstrap";
 import logo from '../images/logo.svg';
 
 export const VERSION = "5.3.10";
-const UPDATED = "07-Aug-2024";
+const UPDATED = "17-Aug-2024";
 
 /* Navbar component that displays at the top of the page. */
 
@@ -25,6 +28,9 @@ export function TopNavbar({
                             setGameModeAlert,
                             setShowBuilderModal
                           }) {
+
+  const [showNews, setShowNews] = useState(false);
+
   const handleExportJSON = () => {
     /* Convert the full roster dictionary into a JSON string and save it to the user's clipboard.
     Also notify them with an alert that fades away after 3 seconds. */
@@ -47,7 +53,9 @@ export function TopNavbar({
     }
   }
 
-  return (<Navbar
+  return (
+  <>
+  <Navbar
     style={{minWidth: "1450px"}}
     bg="dark"
     data-bs-theme="dark"
@@ -64,18 +72,26 @@ export function TopNavbar({
                 alt=""
                 style={{width: "325px"}}
               />
-              <span className="p-0 m-0 mt-1" style={{fontSize: "16px"}}>
-                  Unofficial | v{VERSION} | updated {UPDATED}
+              <Stack direction="horizontal" className="mt-2">
+                <span className="p-0 m-0" style={{fontSize: "16px"}}>
+                    Unofficial | v{VERSION} | updated {UPDATED} | 
                 </span>
+              </Stack>
             </Stack>
           </Stack>
         </Stack>
         <Stack style={{width: "750px"}}>
           <Stack className="mt-3" direction="horizontal" gap={3}>
+            <Button
+              variant="dark"
+              className="ms-auto border border-secondary"
+              onClick={() => setShowNews(true)}
+            >
+              <FaQuestion /> New Edition
+            </Button>
             {!gameMode ?
               <Button
                 variant="success"
-                className="ms-auto"
                 disabled={uniqueModels.length === 0}
                 onClick={() => handleGameMode()}
               >
@@ -84,7 +100,6 @@ export function TopNavbar({
             :
               <Button
               variant="success"
-              className="ms-auto"
               onClick={() => handleBacktoBuilder()}
               >
                 <FaHammer/> Builder Mode
@@ -142,5 +157,37 @@ export function TopNavbar({
         </Stack>
       </Stack>
     </Navbar.Brand>
-  </Navbar>);
+  </Navbar>
+  <Offcanvas show={showNews} onHide={() => setShowNews(false)}>
+    <Offcanvas.Header className="border border-secondary" closeButton>
+      <Offcanvas.Title><b>The New Edition of MESBG</b></Offcanvas.Title>
+    </Offcanvas.Header>
+    <Offcanvas.Body>
+      <b>17th August 2024 - Message from avcordaro</b>
+      <br/><br/>
+      Hello everyone!
+      <br/><br/>
+      Games Workshop has announced that a new edition for MESBG will be released in the near future. 
+      You can find one of the latest announcements here, describing some of the incoming changes...
+      <br/><br/>
+      <a target="_blank" href="https://www.warhammer-community.com/2024/08/15/the-road-goes-ever-on-and-on-the-next-edition-of-middle-earth-strategy-battle-game/">The Road Goes Ever On and On… The Next Edition of Middle-earth™ Strategy Battle Game</a>
+      <br/><br/>
+      You can see in the article above that a lot of profiles are being reworked, along with a new array of army lists and various rule changes.
+      This is likely going to result in a very large amount of work for me once it is released, particularly for profile cards. I will definitely be 
+      working hard to bring you updates that support the new edition of the game on this website. 
+      <br/><br/>
+      My plan is to maintain two versions of this list builder, 
+      one for the new edition, and one for the old edition. I may be able to better estimate the time and effort it will require as more details are 
+      released over the coming months. As a matter of tempering expectations, I'd like to disclaim in advance that it could take many weeks to 
+      fully support all of the changes.
+      <br/><br/>
+      In the meantime, thank you for all your support and kind words over the past year. It has meant a great deal to me. 
+      I appreciate your patience for what's to come in future as I transition this website over to supporting the new edition.
+      <br/><br/>
+      Thank you.
+      <br/><br/>
+    </Offcanvas.Body>
+  </Offcanvas>
+  </>
+  );
 }
