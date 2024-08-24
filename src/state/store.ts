4d8c8541a324f4941c3891cd02b1a67fcf18cb27
mhollink <hollink.mh@gmail.com>
2024-08-24 21:14:36 +0200

    Add zustand devtools to make debugging the application state a breeze

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 import { alertSlice, AlertState } from "./alert";
 import { gamemodeSlice, GamemodeState } from "./gamemode";
 import { modalSlice, ModalState } from "./modal";
@@ -17,19 +17,24 @@ export type SliceSet<T> = (state: Partial<T>) => void;
 export type SliceGet = () => AppState;
 export type Slice<T> = (set: SliceSet<T>, get?: SliceGet) => T;
 
-export const useStore = create<AppState, [["zustand/persist", unknown]]>(
-  persist(
-    (set, get) => ({
-      ...modalSlice(set),
-      ...sidebarSlice(set),
-      ...alertSlice(set),
-      ...gamemodeSlice(set, get),
-      ...rosterSlice(set),
-    }),
-    {
-      name: "mesbg-lb-storage",
-      storage: createJSONStorage(() => sessionStorage),
-      partialize: (state) => getStateToPersist(state),
-    },
+export const useStore = create<
+  AppState,
+  [["zustand/devtools", unknown], ["zustand/persist", unknown]]
+>(
+  devtools(
+    persist(
+      (set, get) => ({
+        ...modalSlice(set),
+        ...sidebarSlice(set),
+        ...alertSlice(set),
+        ...gamemodeSlice(set, get),
+        ...rosterSlice(set),
+      }),
+      {
+        name: "mesbg-lb-storage",
+        storage: createJSONStorage(() => sessionStorage),
+        partialize: (state) => getStateToPersist(state),
+      },
+    ),
   ),
 );
