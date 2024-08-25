0702585f575f4627a1c5183d31a014cf4f04540e
mhollink <hollink.mh@gmail.com>
2024-08-25 12:24:23 +0200

    Add empty state functions for roster building

--- src/state/roster/index.ts
+++ src/state/roster/index.ts
@@ -1,13 +1,34 @@
 import { AllianceLevel } from "../../components/constants/alliances.ts";
 import { Faction, FactionType } from "../../types/factions.ts";
 import { Roster } from "../../types/roster.ts";
+import { Unit } from "../../types/unit.ts";
 import { Slice } from "../store.ts";
 import { ModelCountData } from "./models.ts";
 import { updateRoster } from "./roster.ts";
 
+type RosterFunctions = {
+  setRoster: (roster: Roster) => void;
+
+  // Global warband functions
+  addWarband: () => void;
+  deleteWarband: (warbandId: string) => void;
+  duplicateWarband: (warbandId: string) => void;
+
+  // Hero functions
+  assignHeroToWarband: (warbandId: string, heroId: string, hero: Unit) => void;
+  updateHero: (warbandId: string, heroId: string, hero: Unit) => void;
+  deleteHero: (warbandId: string, heroId: string) => void;
+
+  // Unit functions
+  addUnit: () => void;
+  selectUnit: (warbandId: string, unitId: string, unit: Unit) => void;
+  updateUnit: (warbandId: string, unitId: string, unit: Unit) => void;
+  deleteUnit: (warbandId: string, unitId: string) => void;
+  duplicateUnit: (warbandId: string, unitId: string) => void;
+};
+
 export type RosterState = {
   roster: Roster;
-  setRoster: (roster: Roster) => void;
   factionType: FactionType | "";
   factions: Faction[];
   factionMetaData: ModelCountData;
@@ -15,7 +36,7 @@ export type RosterState = {
   armyBonusActive: boolean;
   uniqueModels: string[];
   rosterBuildingWarnings: string[];
-};
+} & RosterFunctions;
 
 const initialState = {
   roster: {
@@ -45,4 +66,27 @@ export const rosterSlice: Slice<RosterState> = (set) => ({
       undefined,
       "SET_ROSTER",
     ),
+
+  addWarband: (): void => set({}, undefined, "ADD_WARBAND"),
+  duplicateWarband: (warbandId: string): void =>
+    set({}, undefined, "DUPLICATE_WARBAND"),
+  deleteWarband: (warbandId: string): void =>
+    set({}, undefined, "DELETE_WARBAND"),
+
+  assignHeroToWarband: (warbandId: string, heroId: string, hero: Unit): void =>
+    set({}, undefined, "ASSIGN_HERO"),
+  updateHero: (warbandId: string, heroId: string, hero: Unit): void =>
+    set({}, undefined, "UPDATE_HERO"),
+  deleteHero: (warbandId: string, heroId: string): void =>
+    set({}, undefined, "DELETE_HERO"),
+
+  addUnit: (): void => set({}, undefined, "ADD_UNIT"),
+  selectUnit: (warbandId: string, unitId: string, unit: Unit): void =>
+    set({}, undefined, "SELECT_UNIT"),
+  updateUnit: (warbandId: string, unitId: string, unit: Unit): void =>
+    set({}, undefined, "UPDATE_UNIT"),
+  duplicateUnit: (warbandId: string, unitId: string): void =>
+    set({}, undefined, "DUPLICATE_UNIT"),
+  deleteUnit: (warbandId: string, unitId: string): void =>
+    set({}, undefined, "DELETE_UNIT"),
 });
