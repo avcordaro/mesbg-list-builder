c40480655c8741756a67cec48b957501eae96d0a
Alex Cordaro <alex_cordaro@hotmail.co.uk>
2024-09-21 12:10:32 +0100

    Export message tweak

--- src/components/alerts/alerts/ExportAlert.tsx
+++ src/components/alerts/alerts/ExportAlert.tsx
@@ -5,6 +5,6 @@ export const ExportAlert = () => (
     <b>JSON string copied to your clipboard.</b>
     <hr />
     You can keep this somewhere safe, such as in a text file, to reload your
-    army list again later using &apos;Import JSON&apos;.
+    army list again later using &apos;Import roster&apos;.
   </Fragment>
 );
