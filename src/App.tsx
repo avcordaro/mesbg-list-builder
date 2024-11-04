82333954a9b056e5acb649b453c94da83879a8b0
mhollink <hollink.mh@gmail.com>
2024-11-04 20:52:08 +0100

    Add storage warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Container, Grid2, Skeleton } from "@mui/material";
+import { AlertTitle, Container, Grid2, Skeleton } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
@@ -22,7 +24,12 @@ export const App = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
 
-  const { activeRoster, setActiveRoster } = useCurrentRosterState();
+  const {
+    activeRoster,
+    setActiveRoster,
+    storageNoticeActive,
+    dismissStorageNotice,
+  } = useCurrentRosterState();
   const { lastOpenedRoster } = useSavedRostersState();
   const [loaded, setLoaded] = useState(false);
 
@@ -61,6 +68,35 @@ export const App = () => {
       <Container maxWidth={false} fixed sx={{ p: 2 }}>
         <Alerts />
         <main>
+          {storageNoticeActive && (
+            <Alert
+              severity="warning"
+              sx={{ m: "0 auto 1rem", maxWidth: "100ch" }}
+              closeText="dismiss"
+              onClose={dismissStorageNotice}
+              icon={false}
+            >
+              <Box sx={{ maxWidth: "90ch" }}>
+                <AlertTitle>
+                  <strong>Important Notice: Data Storage</strong>
+                </AlertTitle>
+                This application uses your browser’s{" "}
+                <em>
+                  <b>local storage</b>
+                </em>{" "}
+                for saving data. Please note that any data saved here will be{" "}
+                <em>
+                  <b>deleted if you clear your browser history or cache</b>
+                </em>
+                . To avoid losing your data, make use of the{" "}
+                <em>
+                  <b>export options</b>
+                </em>{" "}
+                available via the floating action button in the bottom right
+                corner of this page.
+              </Box>
+            </Alert>
+          )}
           {showHistory ? (
             <SavedGameResults />
           ) : (
