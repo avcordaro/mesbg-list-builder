a2c28dbce8a8c85ea44f7b7695819a65a526ecc6
Alex Cordaro <alex_cordaro@hotmail.co.uk>
2024-11-24 13:42:05 +0000

    Domain message typo

--- src/components/modal/modals/MovingMessageModal.tsx
+++ src/components/modal/modals/MovingMessageModal.tsx
@@ -12,7 +12,7 @@ export const MovingMessageModal = () => {
         <Stack gap={2}>
           <Typography variant="h6">A New Path to Tread</Typography>
           <Typography>
-            Hail, noble traveler! We embark on a new journey, and our home has
+            Hail, noble traveller! We embark on a new journey, and our home has
             moved to a new domain:{" "}
             <a href="https://v2018.mesbg-list-builder.com">
               v2018.mesbg-list-builder.com
@@ -23,7 +23,7 @@ export const MovingMessageModal = () => {
 
           <Alert icon={false} variant="outlined" severity="info">
             <Typography>
-              Fear not, noble traveler, for thy rosters may be carried forth
+              Fear not, noble traveller, for thy rosters may be carried forth
               from this realm and into the new – with but a simple export and
               import, your lists shall journey unharmed to the land that awaits.
             </Typography>
